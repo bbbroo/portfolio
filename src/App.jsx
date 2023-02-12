3e17f7b8145736d709f2074c659d021e9875eb53
brookinsbarry <72997929+brookinsbarry@users.noreply.github.com>
2023-02-11 20:46:49 -0600

    Update routes, add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
